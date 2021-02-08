6ab5d05b2c8979708f6ddf4f188e605ec3cfc2d3
Alican4806 <77585878+Alican4806@users.noreply.github.com>
2021-02-08 23:51:18 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<P, C>
+    {
+        P[] Plate;
+        C[] City;
+        public MyDictionary()
+        {
+            Plate = new P[0];
+            City = new C[0];
+        }
+        public void Add(P item1, C item2)
+        {
+            P[] tempKeyArray =  Plate;
+            Plate = new P[Plate.Length + 1];
+
+            C[] tempValueArray = City;
+            City = new C[City.Length + 1];
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                Plate[i] = tempKeyArray[i];
+                City[i] = tempValueArray[i];
+            }
+            Plate[Plate.Length - 1] = item1;
+            City[City.Length - 1] = item2;
+
+        }
+        public void List()
+        {
+            for (int i = 0; i < Plate.Length; i++)
+            {
+                Console.WriteLine(Plate[i] + " is " + City[i]); ;
+            }
+        }
+    } }
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Dictionary = new MyDictionary<int, string>();
+            Dictionary.Add(48, "Muğla");
+
+
+            Dictionary.List();
+        }
+    }
+}
